f9dd14f64488ad71ee2a3277aac5b69069ae2252
kennedykori <kennedykori47@gmail.com>
2016-10-10 13:33:05 +0300

    remove Some Files

--- Fibonacci.py
+++
@@ -1,9 +0,0 @@
-#Fibonacci serries:
-#The sum of the two eleemnts defines the next
-def fibonacci(size):
-	a, b, fibSeries = 0, 1, []
-	while len(fibSeries) < size:
-		fibSeries.append(b)
-		a, b = b, a + b
-
-	return fibSeries
