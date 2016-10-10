44993359afb606bc45372e0a8d13ccaf2c5fc4a8
kennedykori <kennedykori47@gmail.com>
2016-10-10 13:35:14 +0300

    remove Some Files

--- Purrable.py
+++
@@ -1,9 +0,0 @@
-class Purrable(object):
-	"""This represents the behavior of some cats that purr using the default method purr."""
-
-	def __init__(self):
-		super(Purrable, self).__init__()
-
-	def purr(self):
-		"""Makes the cat purr."""
-		print("Purrrrrrr...")
